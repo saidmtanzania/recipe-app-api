55ea39061e8c2c28eae57a10ccdb51986fcd9369
saidmtanzania <saidmtanzania@gmail.com>
2023-02-16 21:48:40 +0300

    python fresher

---
+++ learn.py
@@ -0,0 +1,14 @@
+## String formatter
+name = 'bob'
+Today = 'Wednesday'
+greeting = f"Hello, {name}, Today is {Today}"
+greetings = "Hello, {}, Today is {}"
+
+# print(greeting)
+# print(greetings.format("Rolf", "Tuesday"))
+
+## List, Tuple, set
+List = ['hello', 'Hi', 'Holla'] #can change, remove, add, keep order of element
+Tuple = ('Volla', 'Voddo', 'vello') #cant change, remove, add, can keep order of element
+Set = {'Vodacom', 'Tigo', 'Airtel'} # cant have duplicate elements
+
