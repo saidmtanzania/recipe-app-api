4e942cc1aadbe693d44d6c6cc823fee06a9ea440
saidmtanzania <saidmtanzania@gmail.com>
2023-03-04 19:36:15 +0300

    feat: setup database and its unit test

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+    Calculator function
+"""
+
+
+def add(x, y):
+    """ Add x & y and return result"""
+    return x + y
+
+
+def subtract(x, y):
+    """ Subtract x from y and return result"""
+    return x - y
