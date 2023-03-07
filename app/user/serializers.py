841115d831f273aaf16f7ad981ed359b9ff44f6c
saidmtanzania <saidmtanzania@gmail.com>
2023-03-07 19:16:40 +0300

    feat: setup user creation and its unit test

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,7 @@
 """
 Serializers for the user API view
 """
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
