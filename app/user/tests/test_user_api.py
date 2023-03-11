de3f1e38f00b4b36ea22b33641b8ecc0948db10d
saidmtanzania <saidmtanzania@gmail.com>
2023-03-11 23:38:35 +0300

    docs: url comment

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-
+# Url links
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 ME_URL = reverse('user:me')
