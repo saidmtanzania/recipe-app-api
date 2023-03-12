4b733f9b3fcfa14707d3de7e2d585e0d0f466c1d
saidmtanzania <saidmtanzania@gmail.com>
2023-03-12 11:56:06 +0300

    feat: implementing feat and unit testing for retrieving recipe for authenticated user

---
+++ app/recipe/serializers.py
@@ -0,0 +1,15 @@
+"""
+Serializer for recipe APIs
+"""
+from rest_framework import serializers
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipe objects."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'description', 'link']
+        read_only_fields = ['id',]
+        
---
+++ app/recipe/views.py
@@ -0,0 +1,24 @@
+"""
+Views for the recipe APIs.
+"""
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """ View for Manage recipes APIs."""
+    authentication_classes = [TokenAuthentication,]
+    permission_classes = [IsAuthenticated,]
+    queryset = Recipe.objects.all()
+    serializer_class = serializers.RecipeSerializer
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
