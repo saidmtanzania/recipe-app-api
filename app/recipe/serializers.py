4e512ec69feaa5dc81fc9aabaad7dace91d4b52a
saidmtanzania <saidmtanzania@gmail.com>
2023-03-13 11:45:49 +0300

    feat: implementing other unit testing for recipe app

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -13,3 +13,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes',
                   'price', 'description', 'link', ]
         read_only_fields = ['id', ]
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -10,14 +10,21 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """ View for Manage recipes APIs."""
+    serializer_class = serializers.RecipeDetailSerializer
     authentication_classes = [TokenAuthentication, ]
     permission_classes = [IsAuthenticated, ]
     queryset = Recipe.objects.all()
-    serializer_class = serializers.RecipeSerializer
 
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id', )
+
+    def get_serializer_class(self):
+        """Return he serializer class for request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self. serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe."""
