733772aa3a27f00b4cb9a3f99ef9331ccb011b6e
saidmtanzania <saidmtanzania@gmail.com>
2023-03-15 20:08:42 +0300

    feat: writing unit test for deleting tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -31,7 +31,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
@@ -41,5 +43,3 @@ class TagViewSet(mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.Generi
     def get_queryset(self):
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
-
-
