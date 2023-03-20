458329dfa30301b983dd3f49c06cfd675eb3c035
saidmtanzania <saidmtanzania@gmail.com>
2023-03-20 09:28:29 +0300

    refactor: refactoring code for better reading and understanding

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,7 +18,7 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful."""
         email = 'test@example.com'
-        password = 'test123'
+        password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password,
