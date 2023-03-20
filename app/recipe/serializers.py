d113c10196144f2ee5ab9cfe488f144046377362
saidmtanzania <saidmtanzania@gmail.com>
2023-03-20 09:04:31 +0300

    feat: implementing updating recipe ingredient feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -32,7 +32,7 @@ class RecipeSerializer(serializers.ModelSerializer):
                   'price', 'description', 'link', 'tags', 'ingredients']
         read_only_fields = ['id']
 
-    def _get_or_create(self, tags, recipe):
+    def _get_or_create_tags(self, tags, recipe):
         """Handle getting or creating tags as needed."""
         auth_user = self.context['request'].user
         for tag in tags:
@@ -57,7 +57,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         tags = validated_data.pop('tags', [])
         ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
-        self._get_or_create(tags, recipe)
+        self._get_or_create_tags(tags, recipe)
         self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
@@ -65,9 +65,15 @@ class RecipeSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         """Update recipe."""
         tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
+
         if tags is not None:
             instance.tags.clear()
-            self._get_or_create(tags, instance)
+            self._get_or_create_tags(tags, instance)
+
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
