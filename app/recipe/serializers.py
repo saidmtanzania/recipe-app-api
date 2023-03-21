260d86dc8fec20de82f85064db3c5372c62fa3f2
saidmtanzania <saidmtanzania@gmail.com>
2023-03-21 13:19:30 +0300

    feat: finalizing implementing upload recipe image feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -85,7 +85,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description','image']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
 
 
 class RecipeImageSerializer(serializers.ModelSerializer):
